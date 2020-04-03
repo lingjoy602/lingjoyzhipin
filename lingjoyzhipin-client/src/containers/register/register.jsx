6f4b7aaf15acbcd1a6bdded9a8ce421b0fb020e6
lingjoy602 <61075126+lingjoy602@users.noreply.github.com>
2020-04-03 20:32:59 +0800

    Update register.jsx

--- lingjoyzhipin-client/src/containers/register/register.jsx
+++ lingjoyzhipin-client/src/containers/register/register.jsx
@@ -56,7 +56,7 @@ class Register extends Component {
 
     return (
       <div>
-        <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
+        <NavBar>lingjoy&nbsp;直&nbsp;聘</NavBar>
         <Logo/>
         <WingBlank>
           <List>
